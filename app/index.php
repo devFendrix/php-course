fd9f54a3e9fae78e0c350300e35b29f923afac8c
devFendrix <dev.fendrix@gmail.com>
2024-04-11 09:35:30 +0200

    add last modif

--- app/index.php
+++ app/index.php
@@ -3,23 +3,40 @@ require_once './config/database.php';
 $db = connect();
 
 
-// TODO variables
+$firstname = 'Marc';
+$lastname = 'Baribaud' . ' Dev';
+$age = 27;
+$height = 1.90;
 
+function getInfoVariable($variable): string
+{
+    return 'Type de la variable : ' . gettype($variable) . ' / Valeur : ' . $variable . '<br>';
+}
 
-//TODO boucles
-
-
-
+echo getInfoVariable($firstname);
+echo getInfoVariable($lastname);
+echo getInfoVariable($age);
+echo getInfoVariable($height);
 
 $request = $db->prepare("SELECT * FROM users;");
 $request->execute();
-$students = $request->fetchAll();
+$students = $request->fetchAll(PDO::FETCH_OBJ);
 
-// TODO for
-// TODO while
+$i = 0;
+echo '<br> Boucle avec while';
+while($i < count($students)){
+    echo $students[$i]->firstname . ' / ' . $students[$i]->lastname . ' / ' . $students[$i]->username . '<br>';
+    $i++;
+}
 
+echo '<br> Boucle avec for';
+for ($j = 0; $j < count($students); $j++) {
+    echo $students[$j]->firstname . ' / ' . $students[$j]->lastname . ' / ' . $students[$j]->username . '<br>';
+}
+
+echo '<br> Boucle avec foreach';
 foreach($students as $student){
-    echo $student['firstname'] . ' / ' . $student['lastname'] . ' / ' . $student['username'] . '<br>';
+    echo $student->firstname . ' / ' . $student->lastname. ' / ' . $student->username . '<br>';
 }
 
 ?>
