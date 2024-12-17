c60def20a0e318ee5287659de398ae20c098f82b
devFendrix <dev.fendrix@gmail.com>
2024-12-17 13:59:22 +0100

    create exo

---
+++ app/apartment.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+ini_set('memory_limit', "1024M");
+ini_set('max_execution_time', 600);
+
+require './config/database.php';
+
+$db = connect();
+
+
+/**
+ * Créer un formulaire permettant l'enregistrement d'appartement 
+ */
+?>
+
+
