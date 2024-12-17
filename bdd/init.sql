1f595823b07961f1df0f92f5002bd75a443f454b
favelas <langloisnoah8@gmail.com>
2024-12-17 13:52:19 +0100

    Update init.sql

--- bdd/init.sql
+++ bdd/init.sql
@@ -1,10 +1,16 @@
-CREATE DATABASE IF NOT EXISTS php_course;
+    CREATE TABLE client(
+        id INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        firstname VARCHAR(255),
+        lastname VARCHAR(255)
+    );
 
-USE php_course;
+    CREATE TABLE commande (
+        id INT(11)AUTO_INCREMENT PRIMARY KEY NOT NULL,
+        commande_id INT (11) NOT NULL,
+        price DECIMAL (8,2),
+        FOREIGN KEY (commande_id) REFERENCES client(id)
+    );
 
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    firstname VARCHAR(255),
-    lastname VARCHAR(255),
-    username VARCHAR(255)
-);
+    INSERT INTO client (firstname,lastname) VALUES ('Noah', 'Langlois');
+
+    INSERT INTO commande (commande_id, price) VALUES ('78936437203', ('20,4€'));
